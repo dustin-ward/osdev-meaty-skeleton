7a160e3f62b98dc78c271fc2f439e77e203cfe4b
dustin-ward <wardddustin@gmail.com>
2024-03-20 23:45:34 -0400

    Colour text in tty

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -7,5 +7,17 @@
 
 void kernel_main(void) {
 	terminal_init();
-	printf("Hello from my libc!\n");
+
+	terminal_setcolor(1);
+	printf("H");
+	terminal_setcolor(2);
+	printf("e");
+	terminal_setcolor(3);
+	printf("l");
+	terminal_setcolor(4);
+	printf("l");
+	terminal_setcolor(5);
+	printf("o");
+	terminal_setcolor(15);
+	printf(" from the kernel with freestanding libc!\n");
 }
